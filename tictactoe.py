a054acb0465428c1952fad1a811ce763c3523e1a
Disha <dishapant22@gmail.com>
2022-01-29 10:50:56 +0530

    Co-authored-by: Alya Singh <singhalya30112004@users.noreply.github.com>

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,9 @@
-print("hello, world")
+import random 
+
+def print_game_board(game_board):
+    for i in range(3):
+        print(game_board[i])
+
+game_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]] 
+print_game_board(game_board)
+user = input()
